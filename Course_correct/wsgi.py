2527320d3f54c79c2ca9ca9690bcb55d780b4329
origbo1978 <legendskye124@gmail.com>
2025-11-17 20:06:32 +0500

    Keep WhiteNoise for admin

--- Course_correct/wsgi.py
+++ Course_correct/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Course_correct.settings')
 
 application = get_wsgi_application()
+app = application
